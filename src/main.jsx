3d9074c7579820df9e66c8850f420efd16b3c297
momen <momenhassan@gmail.com>
2024-08-29 00:45:21 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import "flowbite"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
